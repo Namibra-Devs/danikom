1ccd04ee56aedcca894517febb9c44f3796f0422
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-06-19 08:23:00 +0000

    admin panel partials added

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,9 @@
+@if (!request()->routeIs('admin.file-manager'))
+<footer class="footer">
+  <div class="container-fluid">
+    <div class="d-block mx-auto">
+      <p>Copyright &copy; 2024. All rights Reserved by Danikom</p>
+    </div>
+  </div>
+</footer>
+@endif
