63868275b93ff50bece4070d9b0b2ffbc4f3bbb8
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-08-25 22:33:30 +0000

    login page modified'

--- resources/views/frontend/pages/login.blade.php
+++ resources/views/frontend/pages/login.blade.php
@@ -12,29 +12,34 @@
                         social account.</p>
                 </div>
 
-                <div class="mb-3">
-                    <label for="emailaddress" class="form-label">Email Address<span
-                            class="required-field fw-bold text-danger">*</span></label>
-                    <input type="email" class="form-control" id="emailaddress" placeholder="name@example.com" required>
-                </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password<span class="required-field fw-bold text-danger">*</span></label>
-                            <div class="input-group flex-nowrap" tabindex="0">
-                            <input type="password" class="form-control" name="password" placeholder="**********" aria-label="password" aria-describedby="addon-wrapping" required>
-                            <span class="input-group-text" id="addon-wrapping">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
-                                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
-                                  </svg>
-                            </span>
+                @csrf
+                    <div class="mb-3">
+                        <label for="emailaddress" class="form-label ms-1">Email address</label>
+                        <input type="email" class="form-control" id="emailaddress" name="email" placeholder="name@example.com">
+                        @if (Session::has('err'))
+                            <p class="text-danger mb-2 mt-2">{{ Session::get('err') }}</p>
+                        @endif
+                        @error('email')
+                            <p class="text-danger mb-2 mt-2">{{ convertUtf8($message) }}</p>
+                        @enderror
                     </div>
-                   
-                </div>
-                <div class="remember d-flex ms-5 my-2">
-                    <input type="checkbox" name="agree" id="agree" class="me-2">
-                   <span> Remember Me </span>
-                </div>
 
+                    <div class="mb-3">
+                        <label for="password" class="form-label ms-1">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                        @error('password')
+                            <p class="text-danger mb-2 mt-2">{{ convertUtf8($message) }}</p>
+                        @enderror
+                    </div>
+                    <div class="my-3 form-check d-flex justify-content-between mx-2">
+                        <div>
+                            <input type="checkbox" class="form-check-input" id="rememberme">
+                            <label class="form-check-label" for="rememberme">Remember me</label>
+                        </div>
+                        <div>
+                            <a href="#" class="text-decoration-none text-dark">Forgot Password?</a>
+                        </div>
+                    </div>
                 <div class="d-flex my-3 justify-content-center">
                     <button type="button" class="btn btn-login text-light px-4" data-bs-toggle="modal"
                         data-bs-target="#staticBackdrop">Sign In</button>
