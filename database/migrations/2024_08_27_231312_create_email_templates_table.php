0199c0d16187cda2884e43d4de86d71717d7a9df
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-08-27 23:15:19 +0000

    email_templates migratin table created

---
+++ database/migrations/2024_08_27_231312_create_email_templates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('email_templates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email_type', 100)->nullable();
+            $table->text('email_subject')->nullable();
+            $table->longText('email_body')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('email_templates');
+    }
+};
