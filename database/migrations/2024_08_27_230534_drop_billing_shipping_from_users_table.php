1f37bb992f6c4aa279f2d2dd2f4518c8645ec38c
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-08-27 23:08:59 +0000

    modified user migation table

---
+++ database/migrations/2024_08_27_230534_drop_billing_shipping_from_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['billing_username', 'shpping_username']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('billing_username')->nullable();
+            $table->string('shpping_username')->nullable();
+        });
+    }
+};
